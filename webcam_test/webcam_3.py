940d29348e88a4285d35ccc45c23c9c6457cfb5a
tensorplumber <103926252+tensorplumber@users.noreply.github.com>
2023-03-06 07:51:04 +0900

    Update webcam_3.py

--- webcam_test/webcam_3.py
+++ webcam_test/webcam_3.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     cv2.imshow("Webcam", frame)
-    if cv2.waitKey(1) & 0xFF == 27: # use ESC to .............quit.
+    if cv2.waitKey(1) & 0xFF == 27: # use ESC to ..............quit.
         break
 
 cap.release()
